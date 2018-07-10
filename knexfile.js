a8c6fcf2c82955d1d47705810c30abde849fbd47
Matthew Renn <rennmatthewp@gmail.com>
2018-07-10 12:00:00 +0600

    Adds knex config

---
+++ knexfile.js
@@ -0,0 +1,35 @@
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/morning_brief',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  },
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/morning_brief_test',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  }
+};
