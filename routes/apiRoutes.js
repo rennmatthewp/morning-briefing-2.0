9f446543f132e54bf87d12f46d1e3378a24141a3
Matthew Renn <rennmatthewp@gmail.com>
2018-08-30 15:30:59 -0600

    Adds GET all users enpoint and test and GET specific user by id and tests

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,7 +7,24 @@ const database = require('knex')(configuration);
 const router = express.Router();
 
 router.get('/users', (request, response) => {
-  return response.status(200).json('hello world')
-})
+  database('users')
+    .then(users => response.status(200).json(users))
+    .catch(error => response.status(500).json({ error }));
+});
+
+router.get('/users/:id', (request, response) => {
+  const { id } = request.params;
+  database('users')
+    .where('id', id)
+    .then(user => {
+      if (!user[0]) {
+        return response
+          .status(404)
+          .json({ error: `Could not find user with id: ${id}.` });
+      }
+      return response.status(200).json(user[0]);
+    })
+    .catch(error => response.status(500).json(error));
+});
 
 module.exports = { router, database };
--- test/routes.spec.js
+++ test/routes.spec.js
@@ -1,4 +1,4 @@
-const chai = requre('chai');
+const chai = require('chai');
 const chaiHttp = require('chai-http');
 
 const { database } = require('../routes/apiRoutes');
@@ -15,6 +15,49 @@ describe('API Routes', () => {
           done();
         });
       });
+    });
+  });
+
+  describe('GET /api/v1/users', () => {
+    it('should return an array of users', done => {
+      chai
+        .request(server)
+        .get('/api/v1/users')
+        .end((error, response) => {
+          response.should.have.status(200);
+          response.should.be.json;
+          response.body[0].should.have.property('id', 1);
+          done();
+        });
+    });
+  });
+
+  describe('GET /api/v1/users/:id', () => {
+    it('should return a single users by their id', done => {
+      chai
+        .request(server)
+        .get('/api/v1/users/1')
+        .end((error, response) => {
+          response.should.have.status(200);
+          response.should.be.json;
+          response.body.should.be.an('object');
+          response.body.should.have.property('id', 1);
+          response.body.should.have.property('email', 'yo@yoma.com');
+          response.body.should.have.property('firstName', 'yo');
+          response.body.should.have.property('lastName', 'yoma');
+          done();
+        });
+    });
+
+    it('should return an error with status 404 if the user is not found', done => {
+      chai.request(server)
+      .get('/api/v1/users/500')
+      .end((error, response) => {
+        response.should.have.status(404);
+        response.should.be.json;
+        response.body.should.have.property('error', 'Could not find user with id: 500.');
+        done();
+      })   
     });
   });
 });
